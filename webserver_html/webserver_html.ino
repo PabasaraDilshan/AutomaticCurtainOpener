fc83988b82b5e621bd71f8c1f04e7f165eab85ff
PabasaraDilshan <dilshanpabasara@gmail.com>
2022-08-13 01:42:51 +0530

    evaluation

--- webserver_html/webserver_html.ino
+++ webserver_html/webserver_html.ino
@@ -1,16 +1,17 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <WebSocketsClient.h>
 #include <FS.h>
+#include <WebSocketsClient.h>
+
 #include <Stepper.h>
 #define USE_SERIAL Serial
-IPAddress local_IP(192, 168, 1, 184);
+IPAddress local_IP(192, 168, 8, 1);
 // Set your Gateway IP address
-IPAddress gateway(192, 168, 1, 1);
+IPAddress gateway(192, 168, 8, 1);
 IPAddress subnet(255, 255, 0, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(8, 8, 4, 4);
+//IPAddress primaryDNS(8, 8, 8, 8);   //optional
+//IPAddress secondaryDNS(8, 8, 4, 4);
 ESP8266WebServer server;
 Stepper myStepper(200, 12, 13, 5, 16);
 
@@ -19,8 +20,8 @@ String serverURL;
 WebSocketsClient webSocket;
 uint8_t pin_led = 2;
 
-char* ssid = "SLT-ADSL-60AE3";
-char* password = "MH2236311";
+char* ssid = "Dialog 4G 162";
+char* password = "O5nnCDdM";
 char* APssid = "Curtain";
 char* APpassword = "12345678";
 
@@ -79,6 +80,13 @@ function init(){
 </html>
 )=====";
 
+
+int m = 0;// not calibrated
+int width = -1;
+int currentPose = -1;
+
+bool openDirection = +1;
+
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
    String strPay="";
    int i=0;
@@ -86,14 +94,14 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     case WStype_DISCONNECTED:
       USE_SERIAL.printf("[WSc] Disconnected!\n");
       break;
-    case WStype_CONNECTED: {
+    case WStype_CONNECTED: 
       USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
 
       // send message to server when Connected
-      webSocket.sendTXT("Connected");
-    }
+      webSocket.sendTXT("{\"type\":\"connect\", \"isCurtain\":true,\"curtainId\":11223}");
+    
       break;
-    case WStype_TEXT:
+    case WStype_TEXT:{
       USE_SERIAL.printf("[WSc] get text: %s\n", payload);
 
       while(payload[i]){
@@ -101,16 +109,17 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         i++;
         
       }
-      msgHandler(strPay);
+      String resp;
+       resp =  msgHandler(strPay);
       // send message to server
-      // webSocket.sendTXT("message here");
+      webSocket.sendTXT(resp);}
       break;
     case WStype_BIN:
       USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
       hexdump(payload, length);
 
       // send data to server
-      // webSocket.sendBIN(payload, length);
+//      webSocket.sendBIN(payload, length);
       break;
     case WStype_PING:
     // pong will be send automatically
@@ -123,44 +132,65 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     }
 
 }
+
 String msgHandler(String msg){
 
    DynamicJsonDocument doc(1024);
 
   // You can use a String as your JSON input.
   // WARNING: the string in the input  will be duplicated in the JsonDocument.
-  deserializeJson(doc, msg);
-  JsonObject obj = doc.as<JsonObject>();
+    deserializeJson(doc, msg);
+    const size_t CAPACITY = JSON_OBJECT_SIZE(10);
+    StaticJsonDocument<CAPACITY> resdoc;
+    JsonObject res = resdoc.to<JsonObject>();
+    JsonObject obj = doc.as<JsonObject>();
 //  char* type = obj["type"];
 //  const char* typeStr = obj["type"];
 //  int type = str2Int(typeStr);
 //  const char* data;
-  int type = obj["type"];
+Serial.println(msg);
+  int tp = obj["type"];
   int steps;
   int spd;
-  switch(type){
-    case 0:
-     steps = obj["steps"];
-     spd = obj["speed"];
-     Serial.printf("data Message %d\n",steps);
-     rotateStepper(steps,spd);
+  res["case"] = tp;
+  switch(tp){
+    case 0:// close window
+          res["type"] = "from-curtain";
+          res["success"] = true; 
+          res["curtainId"] = obj["curtainId"]; 
+          res["username"] = obj["username"];
+          res["curtainState"] = "closed";
+          if(obj["speed"]){
+            rotateUntilEnd(-1*openDirection,obj["speed"]);  
+          }else{
+            rotateUntilEnd(-1*openDirection,30);  
+          }
+          
+        Serial.printf("Rotate some degree to neg dir \n");
+
      break;
-    case 1:
-      Serial.printf("digital Read %d\n",digitalRead(4));
-      break;
-    case 2:
-      Serial.println("Calibrating");
-      calibrate();
-      
+    case 1:// open window
+          res["type"] = "from-curtain";
+          res["success"] = true; 
+          res["curtainId"] = obj["curtainId"]; 
+          res["username"] = obj["username"];
+          res["curtainState"] = "opened";
+           if(obj["speed"]){
+            rotateUntilEnd(openDirection,obj["speed"]);  
+          }else{
+            rotateUntilEnd(openDirection,30);  
+          }
+          Serial.printf("Rotate some degree to positive dir \n");
       break;
     default:
       Serial.println("Nothing");
       break;
   }
-
+  String out;
+  serializeJson(res, out);
   
-
-  return "Success";
+ Serial.printf("Sent: %s",out);
+  return out;
   
 }
 String chars2Str(char* chars){
@@ -189,27 +219,62 @@ void blinkLED(){
   digitalWrite(pin_led,!digitalRead(pin_led));
   
  }
+void rotateUntilEnd(int dir,int spd){
+      myStepper.setSpeed(spd);
+  // step 1/100 of a revolution:
+  if(dir>=0){
+     while(true){
+      if(digitalRead(4)==0){
+        myStepper.step(-40);
+        break;
+      }
+    myStepper.step(20);
+    yield();
+  }
+  
+  }else{
+     while(true){
+      if(digitalRead(4)==0){
+        myStepper.step(40);
+        break;
+      }
+    myStepper.step(-20);
+    yield();
+  }
+  
+  }
+  
+}
 void rotateStepper(int steps,int spd){
     myStepper.setSpeed(spd);
   // step 1/100 of a revolution:
   if(steps>=0){
      for(int i =0;i<steps;i++){
+      if(digitalRead(4)==0){
+        myStepper.step(-40);
+        break;
+      }
     myStepper.step(20);
     yield();
   }
   
   }else{
      for(int i =0;i<-steps;i++){
+      if(digitalRead(4)==0){
+        myStepper.step(40);
+        break;
+      }
     myStepper.step(-20);
     yield();
   }
   
   }
- 
+
   }
 
 void toggleLED()
 {
+ rotateUntilEnd(1,30);
   digitalWrite(pin_led,!digitalRead(pin_led));
   server.send_P(200,"text/html", webpage);
 }
@@ -219,10 +284,10 @@ void conPage(){
 
 void onWlan(){
         if (server.hasArg("plain")== false){ //Check if body received
- 
+
             server.send(200, "text/plain", "Body not received");
             return;
- 
+
       }
       String data =  server.arg("plain");
       int ind = 0;
@@ -259,10 +324,10 @@ void onWlan(){
 
 void changeServer(){
     if (server.hasArg("plain")== false){ //Check if body received
- 
+
             server.send(200, "text/plain", "Body not received");
             return;
- 
+
       }
       String data =  server.arg("plain");
       int ind = 0;
@@ -311,12 +376,18 @@ void calibrate(){
   
 }
 
-
+int cloop=0;
+void IntCallback(){
+ Serial.print("Stamp(ms): ");
+ Serial.println(millis());
+}
 void setup()
 {
+  
    Serial.begin(9600);
    pinMode(4,INPUT_PULLUP);
   pinMode(pin_led, OUTPUT);
+//  rotateStepper(1,30);
   bool isFs = SPIFFS.begin();
   if(isFs){
     Serial.println("FS success");
@@ -326,6 +397,8 @@ void setup()
   }
   File netConf = SPIFFS.open("/conf.txt","r");
   File serverFile = SPIFFS.open("/server.txt","r");
+//  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+//  boolean res = WiFi.softAP(APssid, APpassword);
   if(!serverFile){
     serverURL = "192.168.1.129";
     
@@ -345,8 +418,9 @@ void setup()
   if(!netConf){
     Serial.println("Error open");
     
-    WiFi.softAP(APssid, APpassword);
+  
     WiFi.softAPConfig(local_IP, gateway, subnet);
+     WiFi.softAP(APssid, APpassword);
   }else{
     
 
@@ -358,7 +432,14 @@ void setup()
          if(c==(int)'\n'){
           break;
          }
-         ssid1+=(char)c;
+         if(c=='+'){
+          ssid1+=" ";
+          
+          }else{
+            ssid1+=(char)c;
+            
+            }
+         
      }
     while(1){
          c = netConf.read();
@@ -368,45 +449,70 @@ void setup()
          pw1+=(char)c;
      } 
 
+    Serial.println(ssid);
+    Serial.println(password);
     Serial.println(ssid1);
     Serial.println(pw1);
-    WiFi.begin(ssid,password);
-    if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("STA Failed to configure");
-  }
+    WiFi.begin(ssid1,pw1);
+//    if (!WiFi.config(local_IP, gateway, subnet)) {
+//    Serial.println("STA Failed to configure");
+//  }
 
   
- 
+  int count = 0;
   while(WiFi.status()!=WL_CONNECTED)
   {
     Serial.print(".");
+    count++;
     delay(500);
+    if(count>50){
+      WiFi.disconnect();
+      Serial.println("Failed to connec wifi");
+      Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+      boolean res = WiFi.softAP(APssid, APpassword);
+      if(res){
+      Serial.print("Hotspot Ready");
+      }else{
+        Serial.print("Hotspot not Ready");
+        
+        }
+        break;
+    }
   }
   Serial.println("");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
     
   }
- 
+
   
   netConf.close();
   
-  
+  delay(1000);
 
   server.on("/",conPage);
   server.on("/toggle",toggleLED);
   server.on("/wlan",onWlan);
   server.on("/change-server",changeServer);
   server.begin();
-  webSocket.begin(serverURL,5000,"/");
+   delay(1000);
+   Serial.print("Connecting to websocket");
+   Serial.print(serverURL);
+  webSocket.begin(serverURL,80,"/");
   webSocket.onEvent(webSocketEvent);
   webSocket.setReconnectInterval(5000);
+   cloop= 0;
 }
 
 void loop()
 {
+//  if(cloop%10){
+//      Serial.printf("Read %d %d\n",cloop, digitalRead(4));
+//  }
 
   webSocket.loop();
-  server.handleClient();
 
+  
+  server.handleClient();
+//  cloop++;
 }
